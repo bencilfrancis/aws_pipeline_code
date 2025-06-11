e9229fc0eafb0d626d48a91f4438f3af20f924e2
bencilfrancis <bencil.francis@gmail.com>
2025-06-11 12:55:24 -0400

    Bypass fingerprint

--- ansible/dynamic_inventory.py
+++ ansible/dynamic_inventory.py
@@ -12,28 +12,32 @@ inventory = {
         "hosts": [data["jenkins_ip"]],
         "vars": {
             "ansible_user": "ubuntu",
-            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+            "ansible_ssh_private_key_file": "../../terraform-key.pem",
+            "ansible_ssh_common_args": "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
         }
     },
     "sonarqube": {
         "hosts": [data["sonarqube_ip"]],
         "vars": {
             "ansible_user": "ubuntu",
-            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+            "ansible_ssh_private_key_file": "../../terraform-key.pem",
+            "ansible_ssh_common_args": "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
         }
     },
     "nexus": {
         "hosts": [data["nexus_ip"]],
         "vars": {
             "ansible_user": "ubuntu",
-            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+            "ansible_ssh_private_key_file": "../../terraform-key.pem",
+            "ansible_ssh_common_args": "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
         }
     },
     "server": {
         "hosts": [data["server_ip"]],
         "vars": {
             "ansible_user": "ubuntu",
-            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+            "ansible_ssh_private_key_file": "../../terraform-key.pem",
+            "ansible_ssh_common_args": "-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
         }
     },
     "_meta": {}    
