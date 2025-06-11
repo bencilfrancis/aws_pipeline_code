ca8cb33268de16648eda476b4d09a9b7d9249d6f
bencilfrancis <bencil.francis@gmail.com>
2025-06-11 12:25:17 -0400

    Added ansible vars with user and private key path

--- ansible/dynamic_inventory.py
+++ ansible/dynamic_inventory.py
@@ -5,23 +5,42 @@ with open("inventory.json") as f:
     data = json.load(f)
 
 inventory = {
+    "all": {
+        "children": ["jenkins", "nexus", "sonarqube"]
+    },
     "jenkins": {
-        "hosts": [data["jenkins_ip"]["value"]]
+        "hosts": [data["jenkins_ip"]["value"]],
+        "vars": {
+            "ansible_user": "ubuntu",
+            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+        }
     },
     "sonarqube": {
-        "hosts": [data["sonarqube_ip"]["value"]]
+        "hosts": [data["sonarqube_ip"]["value"]],
+        "vars": {
+            "ansible_user": "ubuntu",
+            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+        }
     },
     "nexus": {
-        "hosts": [data["nexus_ip"]["value"]]
+        "hosts": [data["nexus_ip"]["value"]],
+        "vars": {
+            "ansible_user": "ubuntu",
+            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+        }
     },
     "server": {
-        "hosts": [data["server_ip"]["value"]]
+        "hosts": [data["server_ip"]["value"]],
+        "vars": {
+            "ansible_user": "ubuntu",
+            "ansible_ssh_private_key_file": "../../terraform-key.pem"
+        }
     },
     "_meta": {
         "hostvars": {
             ip: {
                 "ansible_user": "ubuntu",
-                "ansible_ssh_private_key_file": "../terraform-key.pem"
+                "ansible_ssh_private_key_file": "../../terraform-key.pem"
             }
 
             for ip in [
